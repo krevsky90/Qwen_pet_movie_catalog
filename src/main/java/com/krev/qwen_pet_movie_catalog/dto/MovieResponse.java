4f0ec34d7fe01c1d9203dee08b4d27f57a1d631b
krevsky90 <krevskiigrigorii@gmail.com>
2026-03-23 22:26:30 +0300

    Sprint 4: add calculated fields to MovieResponse and improve MovieMapper (learn mapstruct)

--- src/main/java/com/krev/qwen_pet_movie_catalog/dto/MovieResponse.java
+++ src/main/java/com/krev/qwen_pet_movie_catalog/dto/MovieResponse.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 
 public record MovieResponse(
         Long id,
-        String title,
+        String displayTitle,    // - calculable field. "Название (Год)"
         Integer year,
         String genre,
         String poster,
         String imdbRating,
+        String ratingCategory,  // - calculable field. HIGH / MEDIUM / LOW - based on imdbRating
         String plot,
         String director,
 //        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")    //jackson
--- src/main/java/com/krev/qwen_pet_movie_catalog/mapper/MovieMapper.java
+++ src/main/java/com/krev/qwen_pet_movie_catalog/mapper/MovieMapper.java
@@ -3,15 +3,62 @@ package com.krev.qwen_pet_movie_catalog.mapper;
 import com.krev.qwen_pet_movie_catalog.dto.MovieRequest;
 import com.krev.qwen_pet_movie_catalog.dto.MovieResponse;
 import com.krev.qwen_pet_movie_catalog.entity.Movie;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")  //важно для Spring DI
+@Mapper(componentModel = "spring",  //важно для Spring DI
+        unmappedTargetPolicy = ReportingPolicy.ERROR    //Если ты забудешь поле, то компиляция упадет (Unmapped target property...)
+)
+
 public interface MovieMapper {
+    //явно игнорим те поля, к-ые не приходят из resuestDto в Entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "poster", ignore = true)
+    @Mapping(target = "imdbRating", ignore = true)
+    @Mapping(target = "plot", ignore = true)
+    @Mapping(target = "director", ignore = true)
     Movie toEntity(MovieRequest requestDto);
 
+    @Mapping(target = "displayTitle",
+            expression = "java(buildDisplayTitle(entity.getTitle(), entity.getYear()))")
+    @Mapping(target = "ratingCategory", source = "imdbRating", qualifiedByName = "ratingCategory")
     MovieResponse toDto(Movie entity);
 
+
+    //явно игнорим те поля, к-ые не приходят из resuestDto в Entity
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "poster", ignore = true)
+    @Mapping(target = "imdbRating", ignore = true)
+    @Mapping(target = "plot", ignore = true)
+    @Mapping(target = "director", ignore = true)
     // Если нужно обновлять существующую сущность:
     void updateEntityFromRequest(MovieRequest requestDto, @MappingTarget Movie entity);
+
+    //custom methods
+    default String buildDisplayTitle(String title, Integer year) {
+        if (title == null) return null;
+        if (year == null) {
+            return title;
+        } else {
+            return title + " (" + year + ")";
+        }
+    }
+
+    // т.к. Любой метод вида A → B воспринимается как глобальный конвертер
+    // то необх указывать имя.
+    // ИЛИ четкий маппинг полей:
+    //    @BeanMapping(ignoreByDefault = true)
+    //    @Mapping(target = "title", source = "title")
+    @Named("ratingCategory")
+    default String mapRatingCategory(String imdbRating) {
+        if (imdbRating == null) return "UNKNOWN";
+        try {
+            double rating = Double.parseDouble(imdbRating);
+            if (rating >= 8.0) return "HIGH";
+            if (rating >= 5.0) return "MEDIUM";
+            return "LOW";
+        } catch (NumberFormatException ex) {
+            return "UNKNOWN";
+        }
+
+    }
 }
